e08057726f2eb9f59915501dff78689681f869c1
saefulbarkah <saefulbarkah650@gmail.com>
2024-07-28 17:42:52 +0700

    fix

--- Client/Content/Aki/JavaScript/Game/Manager/ModManager.js
+++ Client/Content/Aki/JavaScript/Game/Manager/ModManager.js
@@ -1,33 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
+'use strict';
+Object.defineProperty(exports, '__esModule', {
   value: !0,
 }),
   (exports.ModManager = void 0);
-const puerts_1 = require("puerts"),
-  UE = require("ue"),
-  Info_1 = require("../../Core/Common/Info"),
-  Log_1 = require("../../Core/Common/Log"),
-  Protocol_1 = require("../../Core/Define/Net/Protocol"),
-  InputSettings_1 = require("../InputSettings/InputSettings"),
-  TeleportController_1 = require("../Module/Teleport/TeleportController"),
-  CreatureController_1 = require("../World/Controller/CreatureController"),
-  ConfirmBoxController_1 = require("../Module/ConfirmBox/ConfirmBoxController"),
-  ConfirmBoxDefine_1 = require("../Module/ConfirmBox/ConfirmBoxDefine"),
-  ScrollingTipsController_1 = require("../Module/ScrollingTips/ScrollingTipsController"),
-  MapController_1 = require("../Module/Map/Controller/MapController"),
-  ModelManager_1 = require("../Manager/ModelManager"),
-  UidView_1 = require("../Module/UidShow/UidView"),
-  UiManager_1 = require("../../Ui/UiManager"),
-  ModCustomTp_1 = require("./ModFuncs/ModCustomTp"),
-  ModUtils_1 = require("./ModFuncs/ModUtils"),
-  ModDebuger_1 = require("./ModFuncs/ModDebuger"),
-  EntityManager_1 = require("./ModFuncs/EntityManager"),
-  NoClip_1 = require("./ModFuncs/NoClip"),
-  ModLanguage_1 = require("./ModFuncs/ModLanguage"),
-  MobVacuum_1 = require("./ModFuncs/MobVacuum"),
+const puerts_1 = require('puerts'),
+  UE = require('ue'),
+  Info_1 = require('../../Core/Common/Info'),
+  Log_1 = require('../../Core/Common/Log'),
+  Protocol_1 = require('../../Core/Define/Net/Protocol'),
+  InputSettings_1 = require('../InputSettings/InputSettings'),
+  TeleportController_1 = require('../Module/Teleport/TeleportController'),
+  CreatureController_1 = require('../World/Controller/CreatureController'),
+  ConfirmBoxController_1 = require('../Module/ConfirmBox/ConfirmBoxController'),
+  ConfirmBoxDefine_1 = require('../Module/ConfirmBox/ConfirmBoxDefine'),
+  ScrollingTipsController_1 = require('../Module/ScrollingTips/ScrollingTipsController'),
+  MapController_1 = require('../Module/Map/Controller/MapController'),
+  ModelManager_1 = require('../Manager/ModelManager'),
+  UidView_1 = require('../Module/UidShow/UidView'),
+  UiManager_1 = require('../../Ui/UiManager'),
+  ModCustomTp_1 = require('./ModFuncs/ModCustomTp'),
+  ModUtils_1 = require('./ModFuncs/ModUtils'),
+  ModDebuger_1 = require('./ModFuncs/ModDebuger'),
+  EntityManager_1 = require('./ModFuncs/EntityManager'),
+  NoClip_1 = require('./ModFuncs/NoClip'),
+  ModLanguage_1 = require('./ModFuncs/ModLanguage'),
+  MobVacuum_1 = require('./ModFuncs/MobVacuum'),
   keys_State = {},
-  ConfigFileName = "KunModConfig.json";
-  
+  ConfigFileName = 'KunModConfig.json';
+
 class ModManager {
   constructor() {
     this.key_State = false;
@@ -73,8 +73,8 @@ class ModManager {
     VacuumCollect: false,
     VacuumRadius: 300,
     AttributeModifier: false,
-    Uid: "100000000",
-    Language: "English",
+    Uid: '100000000',
+    Language: 'English',
     ESP: false,
     ESPRadius: 300,
     ShowMonster: false,
@@ -105,13 +105,13 @@ class ModManager {
     QuestX: 0,
     QuestY: 0,
     QuestZ: 0,
-    AlwaysCrit: false
+    AlwaysCrit: false,
   };
 
   // please set all settings to false, preventing unverified users
 
   static GetGameDir() {
-    return UE.BlueprintPathsLibrary.ProjectDir() + "Binaries/Win64/";
+    return UE.BlueprintPathsLibrary.ProjectDir() + 'Binaries/Win64/';
   }
 
   static CheckConfigExists() {
@@ -148,38 +148,38 @@ class ModManager {
     }
     this.Settings = Config;
     if (!ModLanguage_1.ModLanguage.Langs.includes(this.Settings.Language)) {
-      this.Settings.Language = "English";
+      this.Settings.Language = 'English';
     }
     ModManager.SaveConfig();
   }
 
   static ModStart() {
-    this.AddToggle("GodMode", "F5");
-    this.AddToggle("HitMultiplier", "F6");
-    this.AddToggle("AutoPickTreasure", "F7");
-    this.AddToggle("AutoAbsorbnew", "F8");
-    this.AddToggle("killAura", "F9");
-    this.AddToggle("PerceptionRange", "F10");
-    this.AddToggle("NoCD", "F11");
-    this.AddToggle("PlayerSpeed", "F12");
-    this.AddToggle("CustomTp", "Insert");
-    this.AddToggle("AutoLoot", "NumPadZero");
-    this.AddToggle("AutoDestroy", "NumPadOne");
-    this.AddKey("MarkTp", "t");
-    this.AddKey("QuestTp", "v");
+    this.AddToggle('GodMode', 'F5');
+    this.AddToggle('HitMultiplier', 'F6');
+    this.AddToggle('AutoPickTreasure', 'F7');
+    this.AddToggle('AutoAbsorbnew', 'F8');
+    this.AddToggle('killAura', 'F9');
+    this.AddToggle('PerceptionRange', 'F10');
+    this.AddToggle('NoCD', 'F11');
+    this.AddToggle('PlayerSpeed', 'F12');
+    this.AddToggle('CustomTp', 'Insert');
+    this.AddToggle('AutoLoot', 'NumPadZero');
+    this.AddToggle('AutoDestroy', 'NumPadOne');
+    this.AddKey('MarkTp', 't');
+    this.AddKey('QuestTp', 'v');
   }
 
   static listenModsToggle() {
-    this.listenMod("GodMode", "F5", "GodMode");
-    this.listenMod("HitMultiplier", "F6", "HitMultiplier");
+    this.listenMod('GodMode', 'F5', 'GodMode');
+    this.listenMod('HitMultiplier', 'F6', 'HitMultiplier');
 
-    this.listenMod("AutoPickTreasure", "F7", "AutoPickTreasure");
-    this.listenMod("AutoAbsorbnew", "F8", "AutoAbsorbnew");
-    this.listenMod("killAura", "F9", "killAura");
-    this.listenMod("PerceptionRange", "F10", "PerceptionRange");
-    this.listenMod("NoCD", "F11", "NoCD");
+    this.listenMod('AutoPickTreasure', 'F7', 'AutoPickTreasure');
+    this.listenMod('AutoAbsorbnew', 'F8', 'AutoAbsorbnew');
+    this.listenMod('killAura', 'F9', 'killAura');
+    this.listenMod('PerceptionRange', 'F10', 'PerceptionRange');
+    this.listenMod('NoCD', 'F11', 'NoCD');
 
-    if (this.listenMod("PlayerSpeed", "F12", "PlayerSpeed")) {
+    if (this.listenMod('PlayerSpeed', 'F12', 'PlayerSpeed')) {
       if (this.Settings.PlayerSpeed) {
         EntityManager_1.EntityManager.SetPlayerSpeed(
           this.Settings.playerSpeedValue
@@ -189,7 +189,7 @@ class ModManager {
       }
     }
     if (this.Settings.HasCustomTpFile) {
-      if (this.listenMod("CustomTp", "Insert", "CustomTp")) {
+      if (this.listenMod('CustomTp', 'Insert', 'CustomTp')) {
         if (this.Settings.CustomTp) {
           ModCustomTp_1.ModCustomTp.CustomTpEnable();
         } else {
@@ -203,34 +203,34 @@ class ModManager {
       ModCustomTp_1.ModCustomTp.listenSelect();
       ModCustomTp_1.ModCustomTp.listenDelay();
 
-      if (this.listenKey("ShowTpState", "Delete")) {
+      if (this.listenKey('ShowTpState', 'Delete')) {
         ModCustomTp_1.ModCustomTp.ShowCtpState();
       }
-      if (this.listenKey("PreviousFile", "PageUp")) {
+      if (this.listenKey('PreviousFile', 'PageUp')) {
         ModCustomTp_1.ModCustomTp.SubFile();
       }
-      if (this.listenKey("NextFile", "PageDown")) {
+      if (this.listenKey('NextFile', 'PageDown')) {
         ModCustomTp_1.ModCustomTp.AddFile();
       }
-      if (this.listenKey("PreviousPos", "Up")) {
+      if (this.listenKey('PreviousPos', 'Up')) {
         ModCustomTp_1.ModCustomTp.SubPos();
         ModCustomTp_1.ModCustomTp.GoTp();
       }
-      if (this.listenKey("NextPos", "Down")) {
+      if (this.listenKey('NextPos', 'Down')) {
         ModCustomTp_1.ModCustomTp.AddPos();
         ModCustomTp_1.ModCustomTp.GoTp();
       }
     }
 
-    this.listenMod("AutoLoot", "NumPadZero", "AutoLoot");
-    this.listenMod("AutoDestroy", "NumPadOne", "AutoDestroy");
+    this.listenMod('AutoLoot', 'NumPadZero', 'AutoLoot');
+    this.listenMod('AutoDestroy', 'NumPadOne', 'AutoDestroy');
     // ModDebuger_1.ModDebuger.EnableDebug();
     // if (ModDebuger_1.ModDebuger.Setting.EnableDebug) {
     //   ModDebuger_1.ModDebuger.ListenDebug();
     // }
 
     if (this.Settings.MarkTp && ModUtils_1.ModUtils.IsInMapView()) {
-      if (this.listenKey("MarkTp", "t")) {
+      if (this.listenKey('MarkTp', 't')) {
         let posz = this.Settings.MarkZ;
         if (posz == 0) posz = this.Settings.MarkTpPosZ;
 
@@ -241,7 +241,7 @@ class ModManager {
         );
       }
     }
-    if (this.Settings.QuestTp && this.listenKey("QuestTp", "v")) {
+    if (this.Settings.QuestTp && this.listenKey('QuestTp', 'v')) {
       if (
         this.Settings.QuestX != 0 &&
         this.Settings.QuestY != 0 &&
@@ -303,13 +303,13 @@ class ModManager {
   }
   static ShowFuncStateTip(func, string) {
     string = ModLanguage_1.ModLanguage.ModTr(string);
-    var info = "Unknown";
+    var info = 'Unknown';
     if (this.Settings.hasOwnProperty(func)) var state = this.Settings[func];
     if (state) {
-      info = string + " | " + ModLanguage_1.ModLanguage.ModTr("TEXT_ON");
+      info = string + ' | ' + ModLanguage_1.ModLanguage.ModTr('TEXT_ON');
       this.ShowTip(info);
     } else {
-      info = string + " | " + ModLanguage_1.ModLanguage.ModTr("TEXT_OFF");
+      info = string + ' | ' + ModLanguage_1.ModLanguage.ModTr('TEXT_OFF');
       this.ShowTip(info);
     }
   }
@@ -324,7 +324,7 @@ class ModManager {
     if (this.IsMyKeyUp(key)) {
       if (this.Settings.hasOwnProperty(func)) {
         this.Settings[func] = !this.Settings[func];
-        ModUtils_1.ModUtils.PlayAudio("play_ui_fx_com_count_number");
+        ModUtils_1.ModUtils.PlayAudio('play_ui_fx_com_count_number');
         this.ShowFuncStateTip(func, funcname);
       }
       return true;
@@ -334,7 +334,7 @@ class ModManager {
   static listenKey(desc, key) {
     var press = this.IsMyKeyUp(key);
     if (press) {
-      ModUtils_1.ModUtils.PlayAudio("play_ui_fx_com_count_number");
+      ModUtils_1.ModUtils.PlayAudio('play_ui_fx_com_count_number');
     }
     return press;
   }
@@ -343,7 +343,7 @@ class ModManager {
     TeleportController_1.TeleportController.TeleportToPositionNoLoading(
       new UE.Vector(x, y, z),
       new UE.Rotator(0, 0, 0),
-      "comment/message"
+      'comment/message'
     );
   }
 
@@ -351,7 +351,7 @@ class ModManager {
     TeleportController_1.TeleportController.TeleportToPositionNoLoading(
       tppos,
       new UE.Rotator(0, 0, 0),
-      "comment/message"
+      'comment/message'
     );
   }
 
@@ -371,14 +371,14 @@ class ModManager {
   }
 
   static FuncState(func, string) {
-    if (func) return string + ModLanguage_1.ModLanguage.ModTr("COLOR_ON");
-    else return string + ModLanguage_1.ModLanguage.ModTr("COLOR_OFF");
+    if (func) return string + ModLanguage_1.ModLanguage.ModTr('COLOR_ON');
+    else return string + ModLanguage_1.ModLanguage.ModTr('COLOR_OFF');
   }
 
   static ChangeUid(string) {
     this.Settings.Uid = string;
-    UiManager_1.UiManager.CloseView("UidView");
-    UiManager_1.UiManager.OpenView("UidView");
+    UiManager_1.UiManager.CloseView('UidView');
+    UiManager_1.UiManager.OpenView('UidView');
   }
 }
 exports.ModManager = ModManager;
