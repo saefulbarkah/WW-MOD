62ec312b5d1245366edeadebcd203d8659ec148b
XopH <saefulbarkah650@gmail.com>
2024-08-15 14:39:48 +0700

    WIP 1.2

--- Client/Content/Aki/JavaScript/Game/Module/LogReport/ThinkingAnalyticsReporter.js
+++ Client/Content/Aki/JavaScript/Game/Module/LogReport/ThinkingAnalyticsReporter.js
@@ -1,6 +1,8 @@
 "use strict";
 var _a;
-Object.defineProperty(exports, "__esModule", { value: !0 }),
+Object.defineProperty(exports, "__esModule", {
+  value: !0,
+}),
   (exports.ThinkingAnalyticsReporter = void 0);
 const cpp_1 = require("cpp"),
   puerts_1 = require("puerts"),
@@ -8,111 +10,70 @@ const cpp_1 = require("cpp"),
   Log_1 = require("../../../Core/Common/Log"),
   Stats_1 = require("../../../Core/Common/Stats"),
   BaseConfigController_1 = require("../../../Launcher/BaseConfig/BaseConfigController"),
-  ThinkDataLaunchReporter_1 = require("../../../Launcher/ThinkDataReport/ThinkDataLaunchReporter"),
   EventDefine_1 = require("../../Common/Event/EventDefine"),
   EventSystem_1 = require("../../Common/Event/EventSystem"),
   ControllerHolder_1 = require("../../Manager/ControllerHolder"),
   ModelManager_1 = require("../../Manager/ModelManager");
 class ThinkingAnalyticsReporter {
   static Init() {
-    ThinkDataLaunchReporter_1.ENABLE_THINKING_ANALYTICS &&
-      (EventSystem_1.EventSystem.Add(
-        EventDefine_1.EEventName.OnGetPlayerBasicInfo,
-        ThinkingAnalyticsReporter.Wpi
-      ),
-      EventSystem_1.EventSystem.Add(
-        EventDefine_1.EEventName.LogOut,
-        ThinkingAnalyticsReporter.Kpi
-      ),
-      EventSystem_1.EventSystem.Add(
-        EventDefine_1.EEventName.LoginSuccess,
-        ThinkingAnalyticsReporter.Qpi
-      ));
+    // 注册事件处理函数已被注释掉，因此不会触发任何上报
+    // EventSystem_1.EventSystem.Add(
+    //   EventDefine_1.EEventName.OnGetPlayerBasicInfo,
+    //   ThinkingAnalyticsReporter.mfi
+    // ),
+    // EventSystem_1.EventSystem.Add(
+    //   EventDefine_1.EEventName.LogOut,
+    //   ThinkingAnalyticsReporter.dfi
+    // ),
+    // EventSystem_1.EventSystem.Add(
+    //   EventDefine_1.EEventName.LoginSuccess,
+    //   ThinkingAnalyticsReporter.Cfi
+    // );
   }
   static Report(e, r) {
-    // ThinkDataLaunchReporter_1.ENABLE_THINKING_ANALYTICS &&
-    //   cpp_1.FThinkingAnalyticsForPuerts.Track(e, r);
+    // 上报函数已被注释掉，因此不会发送任何跟踪信息
+    // cpp_1.FThinkingAnalyticsForPuerts.Track(e, r);
   }
 }
 (exports.ThinkingAnalyticsReporter = ThinkingAnalyticsReporter),
-  ((_a = ThinkingAnalyticsReporter).h9 = void 0),
-  (ThinkingAnalyticsReporter.Wpi = () => {
-    var e = ModelManager_1.ModelManager.PlayerInfoModel.GetId();
-    UE.ThinkingAnalytics.Login(e.toString());
-  }),
-  (ThinkingAnalyticsReporter.Kpi = () => {
-    UE.ThinkingAnalytics.Logout();
-  }),
-  (ThinkingAnalyticsReporter.Qpi = () => {
-    if (
-      ControllerHolder_1.ControllerHolder.KuroSdkController.GetIfGlobalSdk()
-    ) {
-      var r = ModelManager_1.ModelManager.LoginModel?.GetServerId();
-      let e = void 0;
-      var n =
-          BaseConfigController_1.BaseConfigController.GetLoginServerAdditionDataById(
-            r
-          ),
-        n =
-          (n?.TDCfg &&
-            ((e = n.TDCfg), Log_1.Log.CheckInfo()) &&
-            Log_1.Log.Info(
-              "Log",
-              3,
-              "使用AdditionData的数数配置",
-              ["ServerId", r],
-              ["AppID", e?.AppID],
-              ["URL", e?.URL]
-            ),
-          BaseConfigController_1.BaseConfigController.GetLoginServerById(r));
-      if (
-        (n?.TDCfg &&
-          ((e = n.TDCfg), Log_1.Log.CheckInfo()) &&
-          Log_1.Log.Info(
-            "Log",
-            3,
-            "使用LoginServer的数数配置",
-            ["ServerId", r],
-            ["AppID", e.AppID],
-            ["URL", e.URL]
-          ),
-        e)
-      ) {
-        var n = e.URL,
-          t = e.AppID;
-        if (UE.ThinkingAnalytics.HasInstanceInitialized(0)) {
-          let e = !1;
-          r && UE.ThinkingAnalytics.GetServerUrl(0) !== n && (e = !0),
-            (e = t && UE.ThinkingAnalytics.GetAppId(0) !== t ? !0 : e) &&
-              UE.ThinkingAnalytics.DestroyInstance(0);
-        }
-        UE.ThinkingAnalytics.InitializeDefaultInsWithURL_Appid(
-          n,
-          t,
-          ThinkDataLaunchReporter_1.EXIT_WAIT_TIME,
-          ThinkDataLaunchReporter_1.MAX_PENDING_LOG,
-          ThinkDataLaunchReporter_1.SEND_HTTP_TIMEOUT,
-          !0,
-          ThinkDataLaunchReporter_1.CALIBRATE_INTERVAL,
-          ThinkDataLaunchReporter_1.CALIBRATE_STOP_TIMER
-        ),
-          UE.ThinkingAnalytics.CalibrateTime(
-            (0, puerts_1.toManualReleaseDelegate)(_a.Xpi)
-          ),
-          Log_1.Log.CheckInfo() &&
-            Log_1.Log.Info("Login", 10, "数数上报实例已重新创建！");
-      } else
-        Log_1.Log.CheckError() &&
-          Log_1.Log.Error("Login", 10, `未找到 ${r} 对应的数数上报配置`);
-    }
-  }),
-  (ThinkingAnalyticsReporter.Xpi = (e) => {
-    UE.ThinkingAnalytics.HasInstanceTimeCalibrated(e) ||
-      (Log_1.Log.CheckInfo() &&
-        Log_1.Log.Info(
-          "LogReport",
-          10,
-          "数数上报时间校准失败，可以因为以下问题导致：1.CDN数数上报配置错误；2.网络原因连接不上。"
-        ));
-  });
+  ((_a = ThinkingAnalyticsReporter).i9 = void 0)
+  // 登录、登出和时间校准的相关函数已被注释掉
+  // (ThinkingAnalyticsReporter.mfi = () => {
+  //   var e = ModelManager_1.ModelManager.PlayerInfoModel.GetId();
+  //   UE.ThinkingAnalytics.Login(e.toString());
+  // }),
+  // (ThinkingAnalyticsReporter.dfi = () => {
+  //   UE.ThinkingAnalytics.Logout();
+  // }),
+  // (ThinkingAnalyticsReporter.Cfi = () => {
+  //   var e, r;
+  //   ControllerHolder_1.ControllerHolder.KuroSdkController.GetIfGlobalSdk() &&
+  //     (UE.ThinkingAnalytics.DestroyInstance(0),
+  //     (e = ModelManager_1.ModelManager.LoginModel?.GetServerId()),
+  //     (r =
+  //       BaseConfigController_1.BaseConfigController.GetLoginServerAdditionDataById(
+  //         e
+  //       ))
+  //       ? (UE.ThinkingAnalytics.InitializeDefaultInsWithURL_Appid(
+  //           r.TDCfg.URL,
+  //           r.TDCfg.AppID,
+  //           !0
+  //         ),
+  //         UE.ThinkingAnalytics.CalibrateTime(
+  //           (0, puerts_1.toManualReleaseDelegate)(_a.gfi)
+  //         ),
+  //         Log_1.Log.CheckInfo() &&
+  //           Log_1.Log.Info("Login", 10, "数数上报实例已重新创建！"))
+  //       : Log_1.Log.CheckError() &&
+  //         Log_1.Log.Error("Login", 10, `未找到 ${e} 对应的数数上报配置`));
+  // }),
+  // (ThinkingAnalyticsReporter.gfi = (e) => {
+  //   UE.ThinkingAnalytics.HasInstanceTimeCalibrated(e) ||
+  //     (Log_1.Log.CheckInfo() &&
+  //       Log_1.Log.Info(
+  //         "LogReport",
+  //         10,
+  //         "数数上报时间校准失败，可以因为以下问题导致：1.CDN数数上报配置错误；2.网络原因连接不上。"
+  //       ));
+  // });
 //# sourceMappingURL=ThinkingAnalyticsReporter.js.map
--- Client/Content/Aki/JavaScript/Game/Module/WaterMask/WaterMaskController.js
+++ Client/Content/Aki/JavaScript/Game/Module/WaterMask/WaterMaskController.js
@@ -18,65 +18,65 @@ class WaterMaskView extends UiControllerBase_1.UiControllerBase {
   static OnAddEvents() {
     EventSystem_1.EventSystem.Add(
       EventDefine_1.EEventName.OnGetPlayerBasicInfo,
-      this.ENo
+      this.Lqr
     ),
       EventSystem_1.EventSystem.Add(
         EventDefine_1.EEventName.SetResolution,
-        this.yNo
+        this.Dqr
       ),
       EventSystem_1.EventSystem.Add(
         EventDefine_1.EEventName.BackLoginView,
-        this.INo
+        this.Rqr
       );
   }
   static OnRemoveEvents() {
     EventSystem_1.EventSystem.Remove(
       EventDefine_1.EEventName.OnGetPlayerBasicInfo,
-      this.ENo
+      this.Lqr
     ),
       EventSystem_1.EventSystem.Remove(
         EventDefine_1.EEventName.SetResolution,
-        this.yNo
+        this.Dqr
       ),
       EventSystem_1.EventSystem.Remove(
         EventDefine_1.EEventName.BackLoginView,
-        this.INo
+        this.Rqr
       );
   }
 }
 (exports.WaterMaskView = WaterMaskView),
-  ((_a = WaterMaskView).TNo = void 0),
-  (WaterMaskView.LNo = 300),
-  (WaterMaskView.DNo = 300),
-  (WaterMaskView.RNo = 30),
-  (WaterMaskView.UNo = 0.09),
-  (WaterMaskView.p2t = 40),
-  (WaterMaskView.ENo = () => {
-    void 0 !== _a.TNo && _a.INo();
+  ((_a = WaterMaskView).Uqr = void 0),
+  (WaterMaskView.Aqr = 1500),//300
+  (WaterMaskView.xqr = 1500),
+  (WaterMaskView.Pqr = 30),
+  (WaterMaskView.wqr = 1),//0.09
+  (WaterMaskView.MOt = 60),
+  (WaterMaskView.Lqr = () => {
+    void 0 !== _a.Uqr && _a.Rqr();
     var e = UiLayer_1.UiLayer.GetLayerRootUiItem(
         UiLayerType_1.ELayerType.WaterMask
       ),
       t =
-        ((_a.TNo = UE.KuroActorManager.SpawnActor(
+        ((_a.Uqr = UE.KuroActorManager.SpawnActor(
           Info_1.Info.World,
           UE.UIContainerActor.StaticClass(),
           MathUtils_1.MathUtils.DefaultTransform,
           void 0
         )),
-        _a.TNo.RootComponent),
+        _a.Uqr.RootComponent),
       e =
         (t.SetDisplayName("WaterMaskContainer"),
-        UE.KuroStaticLibrary.SetActorPermanent(_a.TNo, !0, !0),
-        _a.TNo.K2_AttachRootComponentTo(e),
+        UE.KuroStaticLibrary.SetActorPermanent(_a.Uqr, !0, !0),
+        _a.Uqr.K2_AttachRootComponentTo(e),
         t.GetRootCanvas().GetOwner().RootComponent),
-      i = (e.widget.width % _a.LNo) / 2,
-      r = (e.widget.height % _a.DNo) / 2,
+      i = (e.widget.width % _a.Aqr) / 2,
+      r = (e.widget.height % _a.xqr) / 2,
       n = e.widget.width / 2,
       _ = e.widget.height / 2,
-      s = Math.ceil(e.widget.width / _a.LNo),
-      o = Math.ceil(e.widget.height / _a.DNo),
+      s = Math.ceil(e.widget.width / _a.Aqr),
+      o = Math.ceil(e.widget.height / _a.xqr),
       //v = ModelManager_1.ModelManager.FunctionModel.PlayerId.toString();
-      v = "there is no watermask";
+      v = "there is no watermark";
     for (let a = 0; a < s; a++)
       for (let e = 0; e < o; e++) {
         var E = UE.KuroActorManager.SpawnActor(
@@ -90,22 +90,22 @@ class WaterMaskView extends UiControllerBase_1.UiControllerBase {
             (E.K2_AttachRootComponentTo(t),
             U.SetDisplayName("WaterMaskText"),
             E.GetComponentByClass(UE.UIText.StaticClass()));
-        U.SetFontSize(_a.p2t),
+        U.SetFontSize(_a.MOt),
           U.SetOverflowType(0),
-          U.SetAlpha(_a.UNo),
+          U.SetAlpha(1),
           U.SetFont(UE.LGUIFontData.GetDefaultFont()),
           U.SetText(v),
           U.SetUIRelativeLocation(
-            new UE.Vector(a * _a.LNo - n + i, e * _a.DNo - _ + r, 0)
+            new UE.Vector(a * _a.Aqr - n + i, e * _a.xqr - _ + r, 0)
           ),
-          U.SetUIRelativeRotation(new UE.Rotator(0, _a.RNo, 0)),
+          U.SetUIRelativeRotation(new UE.Rotator(0, _a.Pqr, 0)),
           UE.KuroStaticLibrary.SetActorPermanent(E, !0, !0);
       }
   }),
-  (WaterMaskView.INo = () => {
-    void 0 !== _a.TNo && (_a.TNo.K2_DestroyActor(), (_a.TNo = void 0));
+  (WaterMaskView.Rqr = () => {
+    void 0 !== _a.Uqr && (_a.Uqr.K2_DestroyActor(), (_a.Uqr = void 0));
   }),
-  (WaterMaskView.yNo = () => {
-    _a.INo(), _a.ENo();
+  (WaterMaskView.Dqr = () => {
+    _a.Rqr(), _a.Lqr();
   });
 //# sourceMappingURL=WaterMaskController.js.map
